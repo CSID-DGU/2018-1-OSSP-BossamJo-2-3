c4daa86c6f214cf634edb4df33aba49b9f7fd792
JLGGG <blockboy88@naver.com>
2018-05-14 16:23:38 +0900

    make ap_scanner.sh

---
+++ GetAPInfo/ap_scanner.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+#Create       : 18.5.11
+#Modify       : 18.5.14
+#Student Name : Lim JunSu
+#Student Num  : 2010111661
+#Description  : 현재 연결된 AP의 mac address를 파싱하는 스크립트 문.
+
+#wireless lan chipset mac address
+#apInfo=`cat /sys/class/net/wlp1s0/address`
+
+# -o 옵션은 매칭되는 전체 라인이 아니라 매칭되는 문자열만 추출
+# -E 정규표현식 사용
+
+while :
+do
+	apInfo=`iwconfig | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'`
+	echo $apInfo
+	isInFile=$(cat APInfo.txt | grep -c "$apInfo")
+
+	# *.txt에 mac address가 저장되어 있는지 확인
+	if [ $isInFile -eq 0 ] 
+	then
+		echo "문자열이 존재하지 않습니다 mac주소 저장" 
+		`iwconfig | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' >> APInfo.txt`
+	else
+		echo "mac 주소가 이미 파일에 있습니다"
+	fi
+	
+	sleep 3
+done
+
+
+
+
